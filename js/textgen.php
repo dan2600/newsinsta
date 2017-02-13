2b5ed51b968974faec835213cc265f565d7c6c56
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-02-13 14:44:52 -0500

    updated branch for logo vday card maker

--- js/textgen.php
+++ js/textgen.php
@@ -14,14 +14,14 @@ $tmpfname = tempnam("/temp", "FOO");
 
 $cmd = "convert -background transparent -fill '".$color."' -font '../fonts/".$theFont."' -pointsize ".$pontsize." label:".$text." png:- | convert - -background transparent -rotate 1.5 png:".$tmpfname;
 
-exec($cmd);
+echo exec($cmd);
 
-$fp = fopen($tmpfname, 'rb');
+//$fp = fopen($tmpfname, 'rb');
 
 //header("Content-Type: image/png");
 //header("Content-Length: " . filesize($tmpfname));
 
-echo base64_encode(file_get_contents($tmpfname));
+//echo base64_encode(file_get_contents($tmpfname));
 //echo "convert -size 987x214 -background transparent -fill \"#000000\" -font VH1Replica-Bold.ttf -gravity north caption:\"".$text."\" png:".$tmpfname;
 
 exit;
